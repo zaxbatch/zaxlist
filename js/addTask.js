a5a7388c9fb8114b4fb9e07cb49102c9aacf2f84
Zax <sneakjunkmail@gmail.com>
2024-09-30 02:09:11 -0400

    database view

--- js/addTask.js
+++ js/addTask.js
@@ -36,9 +36,35 @@ function addTask(event) {
 	    alert("Document successfully written!");
 	    console.log("Document successfully written!");
 	    document.getElementById("task-form").reset();
+	    location.reload();
 	})
 	.catch((error) => {
 	    console.error("Error writing document: ", error);
 	});
 
 }
+
+//delete task
+function deleteDB(a) {
+db.collection('tasks').doc(a).delete().then(() => {
+    console.log("Document successfully deleted!");
+    alert("Document successfully deleted!");
+    location.reload();
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
+}
+
+	db.collection("tasks").get().then((querySnapshot) => {
+
+    querySnapshot.forEach((doc) => {
+        //document.getElementById('task-list').innerHTML = (`<h3>${doc.id} </h3><br/><br/> <p>${doc.data().description}</p> <br/><br/> <h4>${doc.data().due}</h4>`);
+        console.log(`${doc.id} => ${doc.data().description} => ${doc.data().due}</h3>`);
+
+			const div = document.createElement("div");
+			div.innerHTML = `<h4>${doc.id} </h4><p>${doc.data().description}</p><p><strong>Due:</strong> ${doc.data().due}</p><br/>
+			<button onclick="deleteDB('${doc.id}')">Delete</button><hr />`;
+			document.getElementById('task-list').appendChild(div);
+
+    });
+});
